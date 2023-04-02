f7817686dc91d19482312184deaa375cc1648aef
laisen <laisen@pinduoduo.com>
2023-04-02 16:42:07 +0800

    feat: 分类器单选框

--- src/pages/DetectSinglePackage/index.tsx
+++ src/pages/DetectSinglePackage/index.tsx
@@ -1,8 +1,33 @@
 import { observer } from "mobx-react";
 import React from "react";
+import { Classifier, PageStore } from "../../store";
+import useStore from "../../hooks/useStore";
+import s from './index.module.scss';
+import { Select } from "antd";
 
 export const  DetectSinglePackage = observer(() => {
+    const { setClassifier } = useStore<PageStore>();
+    
+    const selectClassifier = (classifier: string) => {
+        setClassifier(classifier as Classifier);
+    }
+
     return (
-        <div>detect single package</div>
+        <div className={s.wrapper}>
+            <div className={s.classifier}>
+                <span className={s.classifierLabel}>分类器</span>
+                <Select 
+                    style={{width: '3.3rem'}}
+                    defaultValue={Classifier.SVM}
+                    onChange={selectClassifier}
+                    options={[
+                        {value: Classifier.SVM, label: 'Kernel SVM'},
+                        {value: Classifier.NB, label: 'Naive Bayes'},
+                        {value: Classifier.RF, label: 'Random Forest'},
+                        {value: Classifier.MLP, label: 'Multi Layer Perceptron'}
+                    ]}>
+                </Select>
+            </div>
+        </div>
     );
 });
--- src/store/index.ts
+++ src/store/index.ts
@@ -7,8 +7,77 @@ export enum PageName{
     RESULT_LIST,
 }
 
+export enum Classifier {
+    RF = 'RF',
+    SVM = 'SVM',
+    NB = 'NB',
+    MLP = 'MLP',
+}
+
+export interface PackageFeatureInfo {
+    hasInstallScripts: boolean;
+    containIP: boolean;
+    useBase64Conversion: boolean;
+    useBase64ConversionInInstallScript: boolean;
+    containBase64StringInJSFile: boolean;
+    containBase64StringInInstallScript: boolean;
+    containDomainInJSFile: boolean;
+    containDomainInInstallScript: boolean;
+    containBytestring: boolean;
+    useBuffer: boolean;
+    useEval: boolean;
+    requireChildProcessInJSFile: boolean;
+    requireChildProcessInInstallScript: boolean;
+    accessFSInJSFile: boolean;
+    accessFSInInstallScript: boolean;
+    accessNetworkInJSFile: boolean;
+    accessNetworkInInstallScript: boolean;
+    accessProcessEnvInJSFile: boolean;
+    accessProcessEnvInInstallScript: boolean;
+    accessCryptoAndZip: boolean;
+    accessSensitiveAPI: boolean;
+    containSuspiciousString: boolean;
+    installCommand: string[],
+    executeJSFiles: string[],
+    packageName: string,
+    version: string,
+}
+
+export type Record = {
+    filePath: string;
+    content: {
+       start: {
+          line: number;
+          column: number;
+       },
+       end: {
+          line: number;
+          column: number;
+       },
+    } | string;
+}
+ 
+type RecordFeatureInfo = Omit<PackageFeatureInfo, 'containBase64StringInJSFile' | 'containBase64StringInInstallScript' | 'installCommand' | 'executeJSFiles' | 'packageName' | 'version'>;
+
+export interface DetectResultDetail {
+    packageName: string;
+    version: string;
+    path: string;
+    size: number; // 单位字节
+    isMalicious: boolean;
+    featurePos: {
+        [k in keyof RecordFeatureInfo]: Record[]
+    }
+}
+
 export class PageStore {
     currentPage = PageName.DETECT_SINGLE_PACKAGE;
+
+    classifier = Classifier.SVM;
+
+    detectResultDetail: DetectResultDetail | null = null;
+
+    resultList: DetectResultDetail[] = [];
 
     constructor() {
         makeAutoObservable(this);
@@ -16,5 +85,9 @@ export class PageStore {
 
     setPageName = (pageName: PageName) => {
         this.currentPage = pageName;
+    }
+
+    setClassifier = (classifier: Classifier) => {
+        this.classifier = classifier;
     }
 }
